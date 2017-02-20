4c20aade201c51ffc64533fac871cc75f035ad22
robnagler <github@q33.us>
2017-02-20 04:32:05 +0000

    travis_wait doesn't work; added noise maker to build.sh

--- container-conf/build.sh
+++ container-conf/build.sh
@@ -3,5 +3,16 @@ build_image_base=radiasoft/python2
 
 build_as_run_user() {
     cd "$build_guest_conf"
+    local noise_pid
+    if [[ $TRAVIS == true ]]; then
+        while true; do
+            echo "$(date): some noise for travis"
+            sleep 600
+        done &
+        noise_pid=$!
+    fi
     . ./codes.sh
+    if [[ -n $noise_pid ]]; then
+        kill -9 "$noise_pid"
+    fi
 }
