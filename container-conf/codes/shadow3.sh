a99cbccd73a168662c0d1af345ce63e2f96f0287
robnagler <github@q33.us>
2017-05-23 22:12:07 +0000

    srio/shadow3 setup.py works so use it

--- container-conf/codes/shadow3.sh
+++ container-conf/codes/shadow3.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #part1: codes_dependencies common
 codes_dependencies xraylib
-codes_download robnagler/shadow3
+codes_download srio/shadow3
 codes_patch_requirements_txt
 python setup.py install
