c9fb496266e1aaf0e7368e8a118ca0068863f85c
robnagler <github@q33.us>
2017-07-25 19:13:35 +0000

    pass code

--- container-conf/build.sh
+++ container-conf/build.sh
@@ -4,5 +4,5 @@ build_travis_trigger_next=( radiasoft/sirepo beamsim-jupyter )
 
 build_as_run_user() {
     cd "$build_guest_conf"
-    curl radia.run | codes_dir="$(pwd)/codes" bash -s srw rslinac rsbeams
+    curl radia.run | codes_dir="$(pwd)/codes" bash -s code srw rslinac rsbeams
 }
