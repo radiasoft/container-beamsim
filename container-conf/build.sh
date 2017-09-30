632a8a192790aab93f93625f56d6b3e525589787
robnagler <github@q33.us>
2017-09-30 02:02:18 +0000

    codes_dir was not valid

--- container-conf/build.sh
+++ container-conf/build.sh
@@ -4,5 +4,5 @@ build_travis_trigger_next=( radiasoft/sirepo beamsim-jupyter )
 
 build_as_run_user() {
     cd "$build_guest_conf"
-    curl radia.run | codes_dir="$(pwd)/codes" bash -s code pyOPALTools warp srw rslinac rsbeams
+    curl radia.run | bash -s code pyOPALTools warp srw rslinac rsbeams
 }
