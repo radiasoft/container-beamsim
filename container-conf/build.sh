0cd182f60bbe92161b925175373d52080dff9497
robnagler <github@q33.us>
2017-11-30 14:37:38 +0000

    build_is_public

--- container-conf/build.sh
+++ container-conf/build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 build_image_base=radiasoft/beamsim-part2
 build_travis_trigger_next=( radiasoft/sirepo beamsim-jupyter )
+build_is_public=1
 
 build_as_run_user() {
     cd "$build_guest_conf"
