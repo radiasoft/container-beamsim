407f50161eed63d68aa18a9d647eb344164b5290
robnagler <github@q33.us>
2018-08-29 20:52:49 +0000

    use beamsim-codes installer

--- container-conf/build.sh
+++ container-conf/build.sh
@@ -1,20 +1,8 @@
 #!/bin/bash
-build_image_base=radiasoft/beamsim-part2
+build_image_base=radiasoft/python2
 build_travis_trigger_next=( radiasoft/sirepo beamsim-jupyter )
 build_is_public=1
 
 build_as_run_user() {
-    cd "$build_guest_conf"
-    local codes=(
-        elegant
-        jspec
-        opal
-        rsbeams
-        rslinac
-        shadow3
-        srw
-        synergia
-        warp
-    )
-    install_repo_eval code "${codes[@]}"
+    install_repo_eval beamsim-codes
 }
