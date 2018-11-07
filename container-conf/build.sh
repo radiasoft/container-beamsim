7ac526c37965cd3308ff6f4ac3c6e6fde4edae42
robnagler <github@q33.us>
2018-11-07 21:58:18 +0000

    use radiasoft/fedora base image

--- container-conf/build.sh
+++ container-conf/build.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-build_image_base=radiasoft/python2
+build_image_base=radiasoft/fedora
 build_travis_trigger_next=( radiasoft/sirepo beamsim-jupyter )
 build_is_public=1
 
 build_as_run_user() {
-    install_repo_eval beamsim-codes
+    # Make sure permissions after home-env are right
     build_run_user_home_chmod_public
+    # now install beasim-codes which should already be public
+    install_repo_eval beamsim-codes
 }
