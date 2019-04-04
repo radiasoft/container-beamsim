085b47602f40363581f06d584a17a55668ef87cd
robnagler <github@q33.us>
2019-04-04 20:58:06 +0000

    comments

--- container-conf/build.sh
+++ container-conf/build.sh
@@ -4,8 +4,8 @@ build_travis_trigger_next=( radiasoft/sirepo beamsim-jupyter )
 build_is_public=1
 
 build_as_run_user() {
-    # Make sure permissions after home-env are right
+    # Make sure permissions after home-env are public
     build_run_user_home_chmod_public
-    # now install beasim-codes which should already be public
+    # Install beamsim-codes which should already be public
     install_repo_eval beamsim-codes
 }
