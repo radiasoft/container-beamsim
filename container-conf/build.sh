c451bf893bf1bc3bef605d6befe4f58e814907c5
robnagler <github@q33.us>
2020-04-15 23:31:14 +0000

    remove build_run_user_home_chmod_public

--- container-conf/build.sh
+++ container-conf/build.sh
@@ -4,8 +4,7 @@ build_travis_trigger_next=( radiasoft/sirepo beamsim-jupyter )
 build_is_public=1
 
 build_as_run_user() {
-    # Make sure permissions after home-env are public
-    build_run_user_home_chmod_public
+    umask 022
     # Install beamsim-codes which should already be public
     install_repo_eval beamsim-codes
 }
