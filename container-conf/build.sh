e47ffdb008f90b967b84476dba045a4ac6592b7f
robnagler <github@q33.us>
2020-11-20 20:54:03 +0000

    umask is 022 by default
    see https://github.com/radiasoft/containers/issues/94

--- container-conf/build.sh
+++ container-conf/build.sh
@@ -4,7 +4,6 @@ build_travis_trigger_next=( radiasoft/sirepo beamsim-jupyter )
 build_is_public=1
 
 build_as_run_user() {
-    umask 022
     # Install beamsim-codes which should already be public
     install_repo_eval beamsim-codes
 }
